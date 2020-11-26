d627cf1fc6646a2c15310ee957da7edd35304db3
Namit Sharda <namit.sharda63@gmail.com>
2020-11-26 18:52:27 +0530

    bug fix #2

--- LocalLibrary/settings.py
+++ LocalLibrary/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'LocalLibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'locallibrary',
-        'USER': 'libuser',
-        'PASSWORD': 'namit@123',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': os.environ.get('SQL_ENGINE', "django.db.backends.sqlite3"),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.environ.get('SQL_USER', "user"),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', "password"),
+        'HOST': os.environ.get("SQL_HOST",'localhost'),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
 
